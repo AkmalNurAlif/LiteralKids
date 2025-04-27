ddac6d8d6ed5306fe63ad004512413494d66c40a
unknown <viaalvia72@gmail.com>
2025-04-28 00:37:33 +0700

    Initial commit LiteralKids

---
+++ app/build.gradle.kts
@@ -0,0 +1,99 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven ( url = "https://jitpack.io" )
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.literalkids"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.literalkids"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    viewBinding{
+        enable = true
+    }
+    dataBinding{
+        enable = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures{
+        compose = true
+    }
+}
+
+dependencies {
+    val lifecycle_version = "2.8.7"
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    //BottomNavbar
+    implementation ("com.github.qamarelsafadi:CurvedBottomNavigation:0.1.3")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    implementation ("com.google.accompanist:accompanist-pager:0.32.0")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.32.0")
+
+    // Compose & Material 3
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation ("androidx.compose.material:material-icons-extended:1.6.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0-alpha09")
+    implementation("androidx.compose.ui:ui:1.6.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.2")
+
+    // Navigation Compose (PENTING!)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/literalkids/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.literalkids
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.literalkids.navigation.Screen
+import com.example.literalkids.ui.LoginUI
+import com.example.literalkids.ui.RegisterUI
+import com.example.literalkids.ui.UIsubcription
+import com.example.literalkids.ui.HomepageUI
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainNavigation()
+        }
+    }
+}
+
+@Composable
+fun MainNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+
+        // Halaman Login
+        composable(Screen.Login.route) {
+            LoginUI(navController = navController)
+        }
+
+        // Halaman Register
+        composable(Screen.Register.route) {
+            RegisterUI(navController = navController)
+        }
+
+        // Halaman Subscription
+        composable(Screen.Subscription.route) {
+            UIsubcription(navController = navController)
+        }
+
+        // Halaman Homepage
+        composable(Screen.Homepage.route) {
+            HomepageUI()
+        }
+
+
+    }
+}
